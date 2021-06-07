81a97a5b758955e1d2ef9f745e4d1b8ab3c7135d
Nikhil Nair <niknair3189@gmail.com>
2021-06-07 23:12:21 +0530

    Updated README

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 * General
     - [ ] Figure out how to avoid seeing login page on consecutive loads
 * Home page
-        - [x] Center brainFart text
+    - [ ] Have dlete confirm show up as popup instead of full screen
     - [ ] Have footer stay at botom of page even while searching
+        - [x] Center brainFart text
         - [x] Added delete confirmation component
         - [x] Add site logo
         - [x] Style the logout button
@@ -15,17 +16,15 @@
         - [x] Add twitter authentication
         - [x] Add anon button 
         - [x] If auth skipped don't allow adding ideas only reading
-* User Component
-    - [ ] Add site logo somewhere?
-    - [ ] Find a way to get high res user icon
-    - [ ] Allow for changing user ids?
+* Search
+    - [ ] Add customizable filter to sort ideas
 * AddIdea
-    - [ ] Add editable category tag to all posts
     - [ ] Have textarea height increase automatically
         - [x] Have inputtext element height increase for long texts
         - [x] Check why user context here shows null even though it prints in other scripts
         - [x] Add conditional render if user.isAnon is true
 * Idea card
+    - [ ] Fix error where one can upvote infinitely by upvoting then downvoting and undownvoting
         - [x] Save upvote or downvote to show as changed button color from load
         - [x] Add functionality to up/down vote buttons considering 1 user can only submit 1 up/down vote for an idea to avoid spamming.
         - [x] Figure out how to update upvote on only 1 idea and not pull full database to avoid re-render of whole list
--- src/components/UserDropdown/UserDropdown.js
+++ src/components/UserDropdown/UserDropdown.js
@@ -6,16 +6,6 @@ import './UserDropdown.scss';
 const UserDropdown = () => {
     const history = useHistory();
 
-    const goToLink = (url) => {
-        auth.signOut().then(()=> {
-            console.log(`USER res\n auth: ${JSON.stringify(auth)}\n `);
-            history.push('/');
-        }).catch((error) => {
-            console.log(`USER error\n error.message: ${JSON.stringify(error.message)}\n `);
-        })
-        console.log(`USER initial\n auth: ${JSON.stringify(auth)}\n `);
-    }
-
     const logOut = () => {
         auth.signOut().then(()=> {
             console.log(`USER res\n auth: ${JSON.stringify(auth)}\n `);
@@ -28,9 +18,8 @@ const UserDropdown = () => {
 
     function DropdownItem(props) {
         return (
-            <a href={props.link} className="menu_item">
+            <a href={props.link} target={props.newtab} className="menu_item">
                 <img className="icon_img" src={props.leftIcon} alt="icon"/>
-                {/* <span className="icon_right">{props.rightIcon}</span> */}
                 {props.children}
             </a>
         );
@@ -38,8 +27,8 @@ const UserDropdown = () => {
 
     return (
         <div className="userdrop">
-            <DropdownItem leftIcon="./images/about_icon.png" link="#">About</DropdownItem>
-            <DropdownItem leftIcon="./images/privacy_icon.png" link="https://docs.google.com/document/d/14Q2jeHABjQZgS-B4zeXYWJ7g1-WPgBSSEIqeVNpG414/edit?usp=sharing">Privacy Policy</DropdownItem>
+            <DropdownItem leftIcon="./images/about_icon.png" newtab='' link="#">About</DropdownItem>
+            <DropdownItem leftIcon="./images/privacy_icon.png" newtab='_blank' link="https://docs.google.com/document/d/14Q2jeHABjQZgS-B4zeXYWJ7g1-WPgBSSEIqeVNpG414/edit?usp=sharing">Privacy Policy</DropdownItem>
             <a href="#" className="menu_item" onClick={logOut}>Signout</a>
         </div>
     );
