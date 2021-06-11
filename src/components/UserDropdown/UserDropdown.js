0eebedba53b22354443d8d74c85bb416460a1947
Nikhil Nair <niknair3189@gmail.com>
2021-06-11 19:23:27 +0530

    Fixed user dropdown items and links

--- src/components/UserDropdown/UserDropdown.js
+++ src/components/UserDropdown/UserDropdown.js
@@ -16,7 +16,6 @@ const UserDropdown = () => {
     function DropdownItem(props) {
         return (
             <a href={props.link} target={props.newtab} className="menu_item">
-                <img className="icon_img" src={props.leftIcon} alt="icon"/>
                 {props.children}
             </a>
         );
@@ -24,9 +23,8 @@ const UserDropdown = () => {
 
     return (
         <div className="userdrop">
-            <DropdownItem leftIcon="./images/about_icon.png" newtab='' link="#">About</DropdownItem>
-            <DropdownItem leftIcon="./images/privacy_icon.png" newtab='_blank' link="https://docs.google.com/document/d/14Q2jeHABjQZgS-B4zeXYWJ7g1-WPgBSSEIqeVNpG414/edit?usp=sharing">Privacy Policy</DropdownItem>
-            <p className="menu_item" onClick={logOut}>Signout</p>
+            <DropdownItem newtab='_blank' link="https://drive.google.com/file/d/1SsWLGNYQczc27lS7rkqQ-wRzAWuFKXTp/view?usp=sharing">Privacy Policy</DropdownItem>
+            <p className="main_menu_item" onClick={logOut}>Signout</p>
         </div>
     );
 }
